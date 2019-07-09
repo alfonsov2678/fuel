2ad577a95a2e71fb9c384ea67d7c70b628fd8499
Eduardo Velasco <eduardo@ordermygear.com>
2019-07-08 22:33:02 -0500

    le doy clases al rey de for loops

--- frontend/src/components/loadScholarships.jsx
+++ frontend/src/components/loadScholarships.jsx
@@ -2,35 +2,37 @@ import React, { Component } from "react";
 import axios from "axios";
 import ScholarshipTable from "./scholarshipsTable";
 import { throwStatement } from "../../node_modules/@babel/types";
+
 class LoadScholarships extends Component {
   state = {
-    scholarships: {},
+    scholarships: [],
     pageSize: 20,
     currentPage: 1,
     selectedArea: null,
     sortColumn: { path: "scholarshipName", order: "asc" },
-    data: [],
     columns: []
   };
+
   handleDelete = movie => {
     const movies = this.state.movies.filter(m => m._id !== movie._id);
     this.setState({ movies });
   };
-  componentDidMount() {
-    axios.get("http://localhost:3000/api/scholarships").then(res => {
-      console.log(res);
-      this.setState({ scholarships: res });
-    });
+
+  async componentDidMount() {
+    const { data }  = await axios.get("http://localhost:3000/api/scholarships");
+    this.setState({ scholarships: data });
   }
+
   handleSort = sortColumn => {
     this.setState({ sortColumn });
   };
+
   render() {
     return (
       <div>
         <h1>Scholarships currently available for you</h1>
         <ScholarshipTable
-          scholarships={this.state.scholarships.data}
+          scholarships={this.state.scholarships}
           onSort={this.handleSort}
           onDelete={this.handleDelete}
           sortColumn={this.state.sortColumn}
